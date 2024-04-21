45c50ebd193bc4625bd34dd073749713c7d81b30
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-04-21 11:58:13 -0500

    up

--- js/jsPages/sayHi.js
+++ js/jsPages/sayHi.js
@@ -2,8 +2,31 @@ import { InyectVantaBirds } from "./vanta.js";
 import { InyectNavBar } from "./navBar.js";
 const vantaBirds = document.querySelector("#vantaBirds");
 const navBar = document.querySelector(".navBar");
+const btn = document.getElementById("buttonFormEmailJS");
+const form = document.getElementById("form");
 
 document.addEventListener("DOMContentLoaded", () => {
   InyectVantaBirds(vantaBirds);
   InyectNavBar(navBar);
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "service_e7uymcv";
+  const templateID = "template_mg02097";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+  form.reset();
 });
